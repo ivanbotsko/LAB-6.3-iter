b60b0914e4b192bfcc2e574dce271070b967f572
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-01 17:47:48 +0200

    first master commit

---
+++ PR 6.3 iter/PR 6.3 iter.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+// Функція для генерації масиву
+std::vector<int> generateArray(int size, int min, int max) {
+    std::vector<int> array;
+    for (int i = 0; i < size; i++) {
+        int randomValue = rand() % (max - min + 1) + min;
+        array.push_back(randomValue);
+    }
+    return array;
+}
+
+// Функція для виведення масиву
+void printElements(const std::vector<int>& array) {
+    std::cout << "--------------------------------" << std::endl;
+    for (int i = 0; i < array.size(); i++) {
+        const int element = array[i];
+        std::cout << element << std::endl;
+    }
+    std::cout << "--------------------------------" << std::endl;
+}
+
+// Функція для перетворення масиву в вектор
+std::vector<int> arrayToVector(const std::vector<int>& array) {
+    std::vector<int> elements;
+    for (int i = 0; i < array.size(); i++) {
+        elements.push_back(array[i]);
+    }
+    return elements;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(0)));  // Ініціалізувати генератор випадкових чисел
+
+    std::vector<int> array = generateArray(10, 1, 100);
+    std::cout << "Formed array:";
+    for (int i = 0; i < array.size(); i++) {
+        std::cout << array[i];
+        if (i < array.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+
+    printElements(array);
+
+    std::vector<int> elements = arrayToVector(array);
+    std::cout << "--------------------------------" << std::endl;
+    for (int i = 0; i < elements.size(); i++) {
+        std::cout << elements[i] << std::endl;
+    }
+    std::cout << "--------------------------------" << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "../PR 6.3 iter/PR 6.3 iter.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(GenerateArrayTest) {
+public:
+
+    TEST_METHOD(GeneratesArrayWithCorrectSize) {
+        int size = 10;
+        int min = 1;
+        int max = 100;
+
+        std::vector<int> array = generateArray(size, min, max);
+
+        Assert::AreEqual(size, static_cast<int>(array.size()));
+    }
+
+    TEST_METHOD(GeneratesArrayWithinSpecifiedRange) {
+        int size = 10;
+        int min = 1;
+        int max = 100;
+
+        std::vector<int> array = generateArray(size, min, max);
+
+        for (int element : array) {
+            Assert::IsTrue(element >= min && element <= max);
+        }
+    }
+};
